a7796948bdb7d6e8d7da6b8c6d2cc63419e89bdb
Stevejobjong <58843907+Stevejobjong@users.noreply.github.com>
2023-11-17 11:45:13 +0900

    Update Job.cs

--- TextRpg/Player/Job.cs
+++ TextRpg/Player/Job.cs
@@ -26,7 +26,7 @@ namespace TextRpg.Player
 
         public bool IsDead { get; protected set; }
 
-        public Job(string name, string occupation, int health, float atk, float def, bool isDead)
+        public Job(string name, string occupation, int level, int health, float atk, float def, bool isDead)
         {
             Name = name;
             Occupation = occupation;
